576b80aea9132c295849cda21e665a82b8adb118
jacobxjennings <jacob.jennings1@gmail.com>
2023-01-30 20:59:13 -0500

    updated readme, summary_stats.png, and R script.

--- MechaCarChallenge/MechaCarChallenge.RScript.R
+++ MechaCarChallenge/MechaCarChallenge.RScript.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(dplyr)
-MechaCar <- read.csv(file='/Users/jacob/Desktop/Bootcamp/MechaCar_Statistical_Analysis/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F) #create the MechaCar dataframe
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) # generate a multiple linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar)) #generate summary statistics
-Suspension_Coil <- read.csv(file='/Users/jacob/Desktop/Bootcamp/MechaCar_Statistical_Analysis/Suspension_Coil.csv', check.names=F, stringsAsFactors = F) #create the suspension_coil dataframe
-total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StDev = sd(PSI)) #create a summary statistics table
-lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StDev = sd(PSI)) # create a lot summary statistics table
-t.test(Suspension_Coil$PSI, mu=1500) #t-test on all manufacturing lots
-t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot1")$PSI, mu=1500) #t-test for lot 1
-t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot2")$PSI, mu=1500) #t-test for lot 2
-t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot3")$PSI, mu=1500) #t-test for lot 3
+MechaCar <- read.csv(file='/Users/jacob/Desktop/Bootcamp/MechaCar_Statistical_Analysis/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F) 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) 
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar)) 
+Suspension_Coil <- read.csv(file='/Users/jacob/Desktop/Bootcamp/MechaCar_Statistical_Analysis/Suspension_Coil.csv', check.names=F, stringsAsFactors = F) 
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StDev = sd(PSI)) 
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StDev = sd(PSI)) 
+t.test(Suspension_Coil$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot1")$PSI, mu=1500) 
+t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot2")$PSI, mu=1500) 
+t.test(subset(Suspension_Coil, Manufacturing_Lot== "Lot3")$PSI, mu=1500)
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # MechaCar Statistical Analysis
 
-## Part one: Linear Regression
+## Part One: Linear Regression
+![Linear Regression](https://github.com/jacobxjennings/MechaCar_Statistical_Analysis/blob/main/Resources/linear_regression.PNG)
 
+## Part Two: Summary Stats
+
+## Part Three: T-Test
+
+## Part Four: 
+![All](https://github.com/jacobxjennings/MechaCar_Statistical_Analysis/blob/main/Resources/t-test.PNG)
+![Lot One](https://github.com/jacobxjennings/MechaCar_Statistical_Analysis/blob/main/Resources/lot1.PNG)
+![Lot Two](https://github.com/jacobxjennings/MechaCar_Statistical_Analysis/blob/main/Resources/lot2.PNG)
+![Lot Three](https://github.com/jacobxjennings/MechaCar_Statistical_Analysis/blob/main/Resources/lot3.PNG)
